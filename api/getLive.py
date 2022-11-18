66657f50655a3f1ddf0f3f7d64f2c77c356e7faa
AdiJoost <adi.joost@gmail.com>
2022-11-18 08:05:52 +0100

    wierd stufff

--- api/getLive.py
+++ api/getLive.py
@@ -1,2 +1,37 @@
 import requests
+import os
+
+class Earthquicky():
+
+    BASE_URL = "https://seismicportal.eu/fdsnws/event/1/"
+    BASE_URL2 = "http://earthquake.usgs.gov/fdsnws/event/1"
+
+    
+
+    def __init__(self, startTime, endTime=None):
+        self.startTime = startTime
+        self.endTime = endTime
+        self.data = None
+
+    def GET(self):
+        tail = f"query?limit=10&start={self.startTime}&format=json"
+        tail = "version"
+        fullURL = Earthquicky.BASE_URL + tail
+        cafile = os.path.join(Earthquicky.getSubPath(), "cacert.pem")
+        print(cafile)
+        self.data = requests.get(fullURL, verify=cafile)
+
+    @classmethod
+    def getSubPath(cls):
+        my_path = os.getcwd().split("ubung8", 1)[0]
+        return os.path.join(my_path, "ubung8", "api")
+
+
+def main():
+    earthy = Earthquicky("2022-08-01")
+    earthy.GET()
+    print(earthy.data)
+
+if __name__ == "__main__":
+    main()
 
