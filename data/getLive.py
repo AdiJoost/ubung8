9fabc59aa31e983ae0e46e8775a2baae540d5cdc
AdiJoost <adi.joost@gmail.com>
2022-12-29 16:04:54 +0100

    addedLiveData

--- data/getLive.py
+++ data/getLive.py
@@ -13,21 +13,24 @@ class Earthquicky():
 
     
 
-    def __init__(self, data=None, startTime=None, endTime=None):
+    def __init__(self, rawData=None, startTime=None, endTime=None):
         self.lockPath = self.__getDataPath("earthquakes.csv.lock")
         self.filename = "earthquakes.csv"
+        self.filePath = self.__getDataPath(self.filename)
         self.fileLock = FileLock(self.lockPath, 3600)
         self.startTime = startTime
         self.endTime = endTime
-        self.data = data
+        self.rawData = rawData
         self.request = None
+        self.__refineData()
 
     def GET(self):
         now = datetime.now()
-        tail = f"query?format=csv&starttime=2000-01-01&endtime=2014-01-02"
+        tail = f"query?format=csv&starttime=2014-01-01&endtime=2014-01-02"
         fullURL = Earthquicky.BL + tail
         self.request = requests.get(fullURL, verify=False)
-        self.data = self.request.text
+        self.rawData = self.request.text
+        self.__refineData()
     
     def getDF(self):
         with self.fileLock:
@@ -38,23 +41,70 @@ class Earthquicky():
             df["date"] = pd.to_datetime(df["date"])
             df.loc[df["latd"] == "S", ["lat"]] = -df["lat"]
             df.loc[df["longd"] == "E", ["long"]] = -df["long"]
+            df = df.drop("_id", axis=1)
         return df
 
     def __getDataPath(self, filename):
         my_path = os.getcwd().split("ubung8", 1)[0]
         return os.path.join(my_path, "ubung8", "data", filename)
+    
+    def __refineData(self):
+        if not self.rawData:
+            self.data = None
+            return
+        rows = self.rawData.split("\n")
+        csvData = {}
+        for i in range(1, len(rows) - 1):
+            args = rows[i].split(",")
+            
+            eqID = args[11]
+            date = args[0]
+            lat = abs(float(args[1]))
+            latd = "N" if float(args[1]) >= 0 else "S"
+            long = abs(float(args[2]))
+            longd = "W" if float(args[2]) >= 0 else "E"
+            depth = args[3]
+            mag = args[4]
+            if (float(mag) < 0 or float(depth) < 0):
+                continue
+            csvData[eqID] = (
+                date,
+                lat,
+                latd,
+                long,
+                longd,
+                depth,
+                mag,
+                eqID
+            )
+        self.data = csvData
+    
+    def save(self):
+        if not self.data:
+            return -1
+        with self.fileLock:
+            with open(self.filePath, "a") as file:
+                for key in self.data:
+                    file.write("\n")
+                    file.write(self.__getCSV(self.data[key]))
 
-    def loadCSV():
-        pass
-        
+    def __getCSV(self, entry):
+        returnValue = ""
+        for i in range (len(entry) - 1):
+            returnValue += str(entry[i])
+            returnValue += ","
+        returnValue += entry[-1]
+        return returnValue
 
+    def __removeDoubleID(self):
+        ids = pd.read_csv()
 
 
 def main():
 
     myDUDE = Earthquicky()
-    df = myDUDE.getDF()
-    print(df.head())
+    myDUDE.GET()
+    myDUDE.save()
 
 if __name__ == "__main__":
     main()
---
+++ data/logger.py
@@ -0,0 +1,47 @@
+import os.path
+from datetime import datetime
+
+class Logger():
+    csv_header = \
+    "plotName,gridsize,dally,spawnRate,tunnel,maxSpeed,tunnelSpeedLimit,doubleSpawn\n"
+    MAIN_FOLDER = "ubung8"
+    def __init__(self):
+        pass
+    
+    #This is a simple log system - it is not thread-safe!
+    @classmethod
+    def log(cls,prefix: str, message: str, file="main_log.txt"):
+        entry = f"[{prefix}] {message}"
+        #create correct path to file
+        my_path = os.getcwd().split(Logger.MAIN_FOLDER, 1)[0]
+        my_path = os.path.join(my_path, Logger.MAIN_FOLDER, "plots", file)
+        
+        #write to file
+        if not os.path.exists(my_path):
+            with open(my_path, "w", encoding=("UTF-8")) as f:
+                f.write("Log-File for Air-Tracker. Created:"\
+                        f" {datetime.today().isoformat()}\n\n")
+        with open(my_path, "a", encoding=("UTF-8")) as f:
+            f.write(f"{entry} -- {datetime.today().isoformat()}\n")
+    
+    @classmethod
+    def log_csv(cls, message: tuple, file="earthquakes"):
+        entry = cls.get_entry(message)
+        #create correct path to file
+        my_path = os.getcwd().split(Logger.MAIN_FOLDER, 1)[0]
+        my_path = os.path.join(my_path, Logger.MAIN_FOLDER, "data", f"{file}.csv")
+        
+        #write to file
+        if not os.path.exists(my_path):
+            with open(my_path, "w", encoding=("UTF-8")) as f:
+                f.write(cls.csv_header)
+        with open(my_path, "a", encoding=("UTF-8")) as f:
+            f.write(entry)
+
+    @classmethod
+    def get_entry(cls, message: tuple):
+        return_value = ""
+        for item in message:
+            return_value += f"{item},"
+        return f"{return_value[:-1]}\n"
+
